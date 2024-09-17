e180f8ba808c4775480309f7a3e1540943a069d9
B1essed1 <mrshakh001@gmail.com>
2024-09-17 13:20:07 +0500

    Initial commit

---
+++ src/main/java/uz/blessed/oson/OsonApplication.java
@@ -0,0 +1,13 @@
+package uz.blessed.oson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsonApplication.class, args);
+	}
+
+}
